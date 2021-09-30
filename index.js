9dc9aad70e44bfed487dec2678afed76dbbbf103
Ely Karr <elykarr@gmail.com>
2021-09-30 11:06:49 -0700

    commit tree 617ce999546eaced567ae6221950b113d231b054

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 
 
-const reducer = (previousValue, currentValue) => previousValue + currentValue;
+// const reducer = (previousValue, currentValue) => previousValue + currentValue;
 
 // console.log(batteryBatches.reduce(reducer));
 
-totalBatteries = batteryBatches.reduce(reducer);
+// const totalBatteries = batteryBatches.reduce(reducer);
+
+// reduce((previousValue, currentValue) => { ... } )
+
+// const totalBatteries = batteryBatches.reduce((total, currentValue, i, array) => {
+//     return total + currentValue + (batteryBatches[i+1] > total ? batteryBatches[i+1] : 0)
+// }, 0)
+
+const totalBatteries = batteryBatches.reduce((total, currentValue) => total + currentValue, 0)
+
+// total = total + currentValue
